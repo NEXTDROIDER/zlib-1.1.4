a9b8598bc0caa8d9894218730e63f77aef990266
Coconut Team union <mineearth2026@gmail.com>
2026-05-30 09:58:47 +0300

    Update install.sh

--- installations/install.sh
+++ installations/install.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-URL="https://github.com/NEXTDROIDER/zlib-1.1.4/releases/download/1.1.4/zlib1.1.4-1.1.4-1-x86_64.pkg.tar.zst"
+URL="https://github.com/NEXTDROIDER/zlib-1.1.4/raw/refs/heads/main/installations/zlib1.1.4-1.1.4-1-x86_64.pkg.tar.zst"
 PKG_DIR=$(mktemp -d)
 
 # Automatically remove the temporary directory on exit
 trap 'rm -rf "$PKG_DIR"' EXIT
 
 echo "Downloading package..."
-curl -sSL "$URL" -o "$PKG_DIR/pkg.tar.zst"
+curl -sSL "$URL" -o "$PKG_DIR/zlib.pkg.tar.zst"
 
 echo "Installing package..."
-sudo pacman -U --noconfirm "$PKG_DIR/pkg.tar.zst"
+sudo pacman -U --noconfirm "$PKG_DIR/zlib.pkg.tar.zst"
 
 echo "Installed successfully!"
